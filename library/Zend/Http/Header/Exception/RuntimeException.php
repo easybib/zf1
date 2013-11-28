59c2ffbbb77c390e32bd0f060b3a13c67816ba0a
Richard Wossal <richard@r-wos.org>
2013-11-28 13:17:29 +0100

    update: Http

--- library/Zend/Http/Header/Exception/RuntimeException.php
+++ library/Zend/Http/Header/Exception/RuntimeException.php
@@ -14,24 +14,23 @@
  *
  * @category   Zend
  * @package    Zend_Http
- * @subpackage UserAgent
- * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @subpackage Header_Exception
+ * @version    $Id$
+ * @copyright  Copyright (c) 2005-2011 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
 
-
 /**
- * @see Zend_Http_UserAgent_Exception
+ * @see Zend_Http_Exception
  */
-// require_once 'Zend/Http/UserAgent/Exception.php';
-
+// require_once 'Zend/Http/Exception.php';
 
 /**
+ * @category   Zend
  * @package    Zend_Http
- * @subpackage UserAgent
- * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @subpackage Header_Exception
+ * @copyright  Copyright (c) 2005-2011 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
-class Zend_Http_UserAgent_Storage_Exception extends Zend_Http_UserAgent_Exception
-{
-}
+class Zend_Http_Header_Exception_RuntimeException extends Zend_Http_Exception
+{}
