ac354e3231cf24e8cb7e75ef4fcec3e5e9c342c8
Richard Wossal <richard@r-wos.org>
2013-11-28 13:00:34 +0100

    update: Tool

--- library/Zend/Tool/Project/Context/Zf/ServicesDirectory.php
+++ library/Zend/Tool/Project/Context/Zf/ServicesDirectory.php
@@ -36,53 +36,20 @@ require_once 'Zend/Tool/Project/Context/Filesystem/Directory.php';
  * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
-class Zend_Tool_Project_Context_Zf_TestLibraryNamespaceDirectory extends Zend_Tool_Project_Context_Filesystem_Directory
+class Zend_Tool_Project_Context_Zf_ServicesDirectory extends Zend_Tool_Project_Context_Filesystem_Directory
 {
-
     /**
      * @var string
      */
-    protected $_namespaceName  = '';
+    protected $_filesystemName = 'services';
 
     /**
-     * @var string
-     */
-    protected $_filesystemName = 'library';
-
-    /**
-     * getName()
+     * Defined by Zend_Tool_Project_Context_Interface
      *
      * @return string
      */
     public function getName()
     {
-        return 'TestLibraryNamespaceDirectory';
+        return 'ServicesDirectory';
     }
-
-    /**
-     * init()
-     *
-     * @return Zend_Tool_Project_Context_Zf_TestLibraryNamespaceDirectory
-     */
-    public function init()
-    {
-        $this->_namespaceName  = $this->_resource->getAttribute('namespaceName');
-        $this->_filesystemName = $this->_namespaceName;
-        parent::init();
-        return $this;
-    }
-
-    /**
-     * getPersistentAttributes()
-     *
-     * @return array
-     */
-    public function getPersistentAttributes()
-    {
-        $attributes = array();
-        $attributes['namespaceName'] = $this->_namespaceName;
-
-        return $attributes;
-    }
-
 }
